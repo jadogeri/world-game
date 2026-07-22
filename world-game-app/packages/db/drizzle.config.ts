2a0d959cc3c5d41e502485ceb2736c53e2bfa620
joseph adogeri <josephadogeridev@gmail.com>
2026-07-21 20:16:32 -0500

    Added doppler.yaml

--- world-game-app/packages/db/drizzle.config.ts
+++ world-game-app/packages/db/drizzle.config.ts
@@ -1,27 +1,4 @@
-// packages/db/drizzle.config.ts
-/*
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
-import { defineConfig } from "drizzle-kit";
-
-const url = process.env.TURSO_DATABASE_URL;
-const authToken = process.env.TURSO_AUTH_TOKEN;
-
-const isLocal = url?.startsWith('file:');
-
-export default defineConfig({
-  out: './src/migrations',
-  schema: './src/schema/',
-  dialect: isLocal ? 'sqlite' : 'turso',
-  dbCredentials: isLocal
-    ? { url: url! }
-    : { url: url!, authToken },
-});
-
-*/import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "drizzle-kit";
